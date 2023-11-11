fc463ca5ccbf80d8354b460c69045a054774a9f6
ISMAEL FACO <faco@MBP-di-ISMAEL.lan>
2023-11-11 15:14:31 +0100

    langue add

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,84 @@ const scroll = new LocomotiveScroll({
       document.body.style.color = "#000101";
     }
   });
+
+  const langSec = document.querySelector('.languageSelection');
+  const butti = document.querySelectorAll('a');
+  const desc = document.querySelector('.description');
+  const dev = document.querySelector('.developpeur');
+  const prop = document.querySelector('.propos');
+  const bi = document.querySelector('.avete');
+  const bo = document.querySelector('.discuter');
+  const ba = document.querySelector('.vous');
+  
+
+
+  butti.forEach(el => {
+    el.addEventListener('click', () => {
+
+      const attr = el.getAttribute('language');
+      desc.textContent = data[attr].description;
+      dev.textContent = dd[attr].description;
+      prop.textContent = da[attr].description;
+      bi.textContent = di[attr].description;
+      bo.textContent = dod[attr].description;
+      ba.textContent = dad[attr].description;
+
+
+      
+    })
+  });
+
+  const data = { 
+    "english": 
+    {
+      'description': " Hi! My name is Ismael Koussoube, I am 29 years old and I am currently working as an Angular/Java Developer in Nantes. Passionate about new technologies, I am interested in interface design and their ability to improve business productivity. Through my training and experiences, I have specialized in the field of front-end and I want to deepen my knowledge.",
+      },
+
+    "italian": 
+    { 
+      'description': "Ciao! Mi chiamo Ismael Koussoube, Ho 29 anni e sono attualmente in carica come Sviluppatore Angular/Java a Nantes. Appassionato di nuove tecnologie, sono interessato al design e alle loro capacità di migliorare la produttività delle aziende. Nel corso della mia formazione e delle mie esperienze, mi sono specializzato nel campo del front-end e desidero continuare ad approfondire le mie conoscenze.",
+      },
+
+    'french': 
+    {
+      'description': "Salut! Je m’appelle Ismael Koussoube, j'ai 29 ans et je suis actuellement en poste en tant que Concepteur Développeur Angular/Java à Nantes. Passionné par les nouvelles technologies, je m'intéresse au design d'interface et leurs facultés à améliorer la productivité des entreprises. Au fil de mes formations et de mes expériences, je me suis spécialisé dans le domaine du front-end et je souhaite approfondir mes connaissances.",
+      },
+  }
+  
+
+  const dd = { 
+    "english": 
+    {
+      'description': "Developer",
+      },
+
+    "italian": 
+    { 
+      'description': "Sviluppatore",
+      },
+
+    'french': 
+    {
+      'description': "Développeur",
+      },
+  }
+
+  const da = { 
+    "english": 
+    {
+      'description': "About",
+      },
+
+    "italian": 
+    { 
+      'description': "A proposito",
+      },
+
+    'french': 
+    {
+      'description': "À propos",
+      },
+  }
+  
+  
